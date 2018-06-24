6d574791a6b460221ea8bb223d090999ce66abf8
ThePuffin <ballutvincent@gmail.com>
2018-06-24 17:33:53 +0200

    modification effectué sur cv impression

--- src/components/Projets/Projets.js
+++ src/components/Projets/Projets.js
@@ -6,26 +6,26 @@ class Projets extends Component {
     this.state = {};
   }
   render() {
-    return <div>
+    return (
+      <div>
         <div className="col-sm-12 col-md-12">
-          <h3 className="droite">Projets WEB</h3>
+          <h3 className="droite">Réalisations WEB</h3>
           <p>
             <b>Mademoiselle FLE : </b>
             back-office d'un site d'apprentissage du français
-            <i> - Projet client{" "}
-            </i> 
-          </p> <p>
+            <i> - Projet client </i>
+          </p>{" "}
+          <p>
             <b>La route des vins : </b>
             application mobile d'ajout de contenu pour une application vocale
-            <i> - Projet client
-            </i> 
+            <i> - Projet client</i>
           </p>
           <p>
             <b>Bugcooker : </b>
             site internet d'un restaurant gastronomique autour des insectes
             <i>- Projet fictif</i>
-          </p> 
-           <p>
+          </p>
+          <p>
             <b>Space meat : </b>
             application mobile de rencontre sur le thème de star wars
             <i> - Hackaton</i>
@@ -33,12 +33,11 @@ class Projets extends Component {
           <p>
             <b>Captain JSON : </b>
             encyclopédie WEB sur le thème des super-héros
-            <i>
-               - Hackaton
-            </i> 
+            <i>- Hackaton</i>
           </p>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 

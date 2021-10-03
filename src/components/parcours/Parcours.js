b9f9b97cbfc28e4222f80a1b439a841c9d3663ab
Vinz <ballutvincent@gmail.com>
2021-10-03 19:02:16 +0200

    update experience

--- src/components/parcours/Parcours.js
+++ src/components/parcours/Parcours.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import "./Parcours.css";
+import './Parcours.css';
+
+import React, { Component } from 'react';
 
 class Parcours extends Component {
   constructor(props) {
@@ -7,45 +8,49 @@ class Parcours extends Component {
     this.state = {};
   }
   render() {
-    return <div>
+    return (
+      <div>
         <h3> Parcours professionnel </h3>
         <br />
 
         <div className="row">
           <div className="col-4">
-            <h5> Chargé d'affaires géotechnique : EgSol</h5>
+            <h5> Développeur WEB full-stack</h5>
 
             <p className="descriptionParcours">
-              Préparation de chantier(DICT, plans, ...)<br />
+              Analyse des besoins du client
+              <br />
             </p>
             <p className="descriptionParcours">
-              Gestion des clients<br />
+              Réalisation de conceptions
+              <br />
             </p>
             <p className="descriptionParcours">
-              Réalisation de sondage in-situ<br />
+              Développement front et back
+              <br />
             </p>
-            <p className="descriptionParcours">
-              {" "}
-              Rédaction de rapports de type G1, G2 et G5{" "}
-            </p>
+            <p className="descriptionParcours"> Mise en production </p>
           </div>
           <div className="col-4">
-            <h5>Conducteur de travaux : Lacroix Signalisation </h5>
+            <h5>Technicien en pathologie : CEBTP </h5>
 
             <p className="descriptionParcours">
-              Préparation de chantier<br />
+              Préparation de chantier
+              <br />
             </p>
             <p className="descriptionParcours">
-              Echanges avec les différents intervenants<br />
+              Réalisation de carottages
+              <br />
             </p>
             <p className="descriptionParcours">
-              Suivi du chantier <br />
+              Rédaction de comptes-rendus <br />
             </p>
           </div>
           <div className="col-4">
             <h5> Autres expériences professionnelles</h5>
             <p className="pFont"> Agent d’ accueil : Vulcania </p>
             <p className="pFont">Guide– Animateur : Volcan de Lemptegy </p>
+            <p className="pFont">Technicien de laboratoire : APPIA </p>
           </div>
         </div>
         <br />
@@ -56,12 +61,12 @@ class Parcours extends Component {
             <p className="colle traitVertHaut">|</p>
           </div>
           <div className="col-4 ">
-            <span>Novembre 2005 à Février 2006</span>
+            <span>Avril à mai 2006</span>
             <hr className="petitTrait traitBas" />
             <p className="colle traitVertHaut">|</p>
           </div>
           <div className="col-4 ">
-            <span>Etés 2002 et 2003</span>
+            <span>Avant novembre 2005</span>
             <hr className="petitTrait traitBas" />
             <p className="colle traitVertHaut">|</p>
           </div>
@@ -71,43 +76,50 @@ class Parcours extends Component {
           <div className="offset-2 col-4 ">
             <p className="colle traitVertBas">|</p>
             <hr className="petitTrait traitHaut" />
-            <span>Avril à mai 2006</span>
+            <span> Juin 2006 à mai 2017 </span>
           </div>
           <div className="col-4 ">
             <p className=" colle traitVertBas">|</p>
             <hr className="petitTrait traitHaut" />
-            <span>Avril à Septembre 2005</span>
+            <span>Novembre 2005 à Février 2006</span>
           </div>
         </div>
         <div className="row">
           <div className="offset-2 col-4">
-            <h5>Technicien en pathologie : CEBTP </h5>
+            <h5> Chargé d'affaires géotechnique : EgSol</h5>
 
             <p className="descriptionParcours">
-              Préparation de chantier<br />
+              Préparation de chantier(DICT, plans, ...)
+              <br />
             </p>
             <p className="descriptionParcours">
-              Réalisation de carottages<br />
+              Gestion des clients
+              <br />
             </p>
             <p className="descriptionParcours">
-              Redaction de comptes-rendus<br />
+              Réalisation de sondage in-situ
+              <br />
             </p>
+            <p className="descriptionParcours"> Rédaction de rapports de type G1, G2 et G5 </p>
           </div>
           <div className="col-4">
-            <h5>Technicien de Laboratoire : Appia</h5>
+            <h5>Conducteur de travaux : Lacroix Signalisation </h5>
 
             <p className="descriptionParcours">
-              Contrôle des formules des enrobés<br />
+              Préparation de chantier
+              <br />
             </p>
             <p className="descriptionParcours">
-              Réalisation d'essais de chargement à la plaque<br />
+              Echanges avec les différents intervenants
+              <br />
             </p>
             <p className="descriptionParcours">
-              Rédaction de comptes-rendus des résultats<br />
+              Suivi du chantier <br />
             </p>
           </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
